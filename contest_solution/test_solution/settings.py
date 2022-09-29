6c55fa932811dce57103466971e322042e793ce0
Víctor Ruiz <victorruiz@zyte.com>
2022-09-29 16:44:18 +0200

    Publish contest solution scrapy project

---
+++ contest_solution/test_solution/settings.py
@@ -0,0 +1,2 @@
+BOT_NAME = "test-solution"
+SPIDER_MODULES = ["test_solution.spiders"]
---
+++ contest_solution/test_solution/spiders/items.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
+from itemloaders.processors import Compose, Join, TakeFirst
+from scrapy.loader import ItemLoader
+
+
+@dataclass
+class TestItem:
+    item_id: str = field(default_factory=str)
+    name: str = field(default_factory=str)
+    image_id: Optional[str] = field(default=None)
+    rating: str = field(default_factory=str)
+    phone: str = field(default_factory=str)
+
+
+class TestItemLoader(ItemLoader):
+    default_item_class = TestItem
+    default_output_processor = Compose(Join(), str.strip)
+    image_id_out = TakeFirst()
